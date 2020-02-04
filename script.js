0ea9a0653ae5be277dee9cdce79c8c2539709fe6
Gutierrez <brandongutierrez@my.whccd.edu>
2020-02-03 21:33:22 -0800

    Bleh bleh bleeeh

---
+++ script.js
@@ -0,0 +1,21 @@
+// #1???
+let myName = "Brandon";
+
+const greetMe = (name) => {
+    let myGreeting = "Hello, ";
+    return myGreeting + name + "!";
+};
+console.log(greetMe("Brandon"));
+let greetName = greetMe("Brandon");
+
+// #2???
+const circumference = (diameter) => {
+    const pi = 3.14;
+    return pi * diameter;
+}
+console.log(circumference(10));
+
+//#3???
+
+
+
